40ab335eaae0909a7a31f211d5d462b6bfc9c2fe
SHINYATANAKA123 <123asde.med@gmail.com>
2021-05-04 03:03:40 +0000

    [Add]3-8 機能の修正全体のレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,23 +9,41 @@ class BooksController < ApplicationController
   end
   
   def create
-  book = Book.new(book_params)
-  book.save
+  @book = Book.new(book_params)
+  if @book.save
+    flash[:complete] = "Book was successfully created."
+     redirect_to book_path(@book)
+  else 
+    @books = Book.all
+    render :index
   end
-  
-  
-  def update
-      @book = Book.find(params[:id])
-      @book.update(book_params)
-      redirect_to book_path(@book)
   end
-
   
   def edit
+  @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:complete] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+  else
+   @book = Book.all
+   render :edit
+  end  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+    flash[:complete]="Book was successfully destroyed."
+    redirect_to "/books"
+    end
   end
   
    private
-  def book_params
-    params.require(:book).permit(:title, :category, :body)
-  end
- end 
+   def book_params
+    params.require(:book).permit(:title, :body)
+   end
+   end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-get 'top' => 'homes#top'
-get 'books' => 'books#index'
-get 'books/:id' => 'books#show', as: 'book'
-post 'books' => 'books#create'
-
+root "books#top"
+resources :books
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
